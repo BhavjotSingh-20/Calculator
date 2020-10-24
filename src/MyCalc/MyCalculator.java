c78323a0a11eb6726e826f6d85af9662f1606577
Bhavjot Singh <bhavjot794@gmail.com>
2020-10-24 14:06:49 +0530

    Button Working

--- src/MyCalc/MyCalculator.java
+++ src/MyCalc/MyCalculator.java
@@ -2,18 +2,22 @@ package MyCalc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class MyCalculator {
-    public static void main(String[] args) {
+public class MyCalculator implements ActionListener {
+    JButton one,two,three,four,five,six,seven,eight,nine,decimal,zero,clear,cross,percent,plus,minus,mult,divide,
+            result,no;
+    JTextField tf;
+    MyCalculator() {
         JFrame f = new JFrame("Calculator");
         SwingUtilities.updateComponentTreeUI(f);
         f.invalidate();
         f.validate();
         f.repaint();
-       JButton one,two,three,four,five,six,seven,eight,nine,decimal,zero,clear,cross,percent,plus,minus,mult,divide,
-               result,no;
-        JTextField tf =  new JTextField();
+
+        tf =  new JTextField();
         tf.setBounds(10,10,290,40);
         tf.setBackground(Color.red);
         clear = new JButton("AC");
@@ -56,6 +60,28 @@ public class MyCalculator {
         zero.setBounds(80,330,70,70);
         decimal.setBounds(150,330,70,70);
          result.setBounds(220,330,70,70);
+         clear.addActionListener(this);
+         cross.addActionListener(this);
+        percent.addActionListener(this);
+        divide.addActionListener(this);
+        seven.addActionListener(this);
+        eight.addActionListener(this);
+        nine.addActionListener(this);
+        mult.addActionListener(this);
+        four.addActionListener(this);
+        five.addActionListener(this);
+        six.addActionListener(this);
+        minus.addActionListener(this);
+        one.addActionListener(this);
+        two.addActionListener(this);
+        three.addActionListener(this);
+        plus.addActionListener(this);
+        zero.addActionListener(this);
+        decimal.addActionListener(this);
+        result.addActionListener(this);
+
+
+
         f.add(tf);
         f.add(clear);
         f.add(cross);
@@ -66,6 +92,68 @@ public class MyCalculator {
      f.setSize(300,450);
         f.setLayout(null);
         f.setVisible(true);
+
+
+    }
+    public static void main(String[] args) {
+        new MyCalculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == one) {
+            System.out.println("one pressed");
+            tf.setText(tf.getText() + "1");
+        }
+        else if(e.getSource() == two) {
+            tf.setText(tf.getText() + "2");
+        }
+        else if(e.getSource() == three) {
+            tf.setText(tf.getText() + "3");
+        }
+        else if(e.getSource() == four) {
+            tf.setText(tf.getText() + "4");
+        }
+        else if(e.getSource() == five) {
+            tf.setText(tf.getText() + "5");
+        }
+        else if(e.getSource() == six) {
+            tf.setText(tf.getText() + "6");
+        } else if(e.getSource() == seven) {
+            tf.setText(tf.getText() + "7");
+        } else if(e.getSource() == eight) {
+            tf.setText(tf.getText() + "8");
+        }
+        else if(e.getSource() == nine) {
+            tf.setText(tf.getText() + "9");
+        }
+        else if(e.getSource() == zero) {
+            tf.setText(tf.getText() + "0");
+        }
+        else if(e.getSource() == decimal) {
+            tf.setText(tf.getText() + ".");
+        }
+        else if(e.getSource() == plus) {
+            tf.setText(tf.getText() + "+");
+        }
+        else if(e.getSource() == minus) {
+            tf.setText(tf.getText() + "-");
+        }
+        else if(e.getSource() == percent) {
+            tf.setText(tf.getText() + "%");
+        }
+        else if(e.getSource() == mult) {
+            tf.setText(tf.getText() + "*");
+        }
+        else if(e.getSource() == clear) {
+            tf.setText("");
+        }
+        else if(e.getSource() == cross) {
+            StringBuffer str = new StringBuffer(tf.getText());
+            str.deleteCharAt(str.length()-1);
+            tf.setText(str.toString());
+        }
+
 
 
     }
