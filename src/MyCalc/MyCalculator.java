c93c4b058232f87cfd662518571417d6a07d30c2
Bhavjot Singh <bhavjot794@gmail.com>
2020-10-24 13:35:39 +0530

    UI Setup

--- src/MyCalc/MyCalculator.java
+++ src/MyCalc/MyCalculator.java
@@ -1,4 +1,72 @@
 package MyCalc;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class MyCalculator {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Calculator");
+        SwingUtilities.updateComponentTreeUI(f);
+        f.invalidate();
+        f.validate();
+        f.repaint();
+       JButton one,two,three,four,five,six,seven,eight,nine,decimal,zero,clear,cross,percent,plus,minus,mult,divide,
+               result,no;
+        JTextField tf =  new JTextField();
+        tf.setBounds(10,10,290,40);
+        tf.setBackground(Color.red);
+        clear = new JButton("AC");
+        cross = new JButton("C");
+        percent = new JButton("%");
+        divide =  new JButton("/");
+        seven = new JButton("7");
+        eight=  new JButton("8");
+        nine = new JButton("9");
+        mult = new JButton("*");
+        four = new JButton("4");
+        five = new JButton("5");
+        six = new JButton("6");
+        minus =  new JButton("-");
+        one = new JButton("1");
+        two = new JButton("2");
+        three =  new JButton("3");
+        plus =  new JButton("+");
+        no = new JButton("no");
+        zero =  new JButton("0");
+        decimal = new JButton(".");
+        result=  new JButton("=");
+        clear.setBounds(10,50,70,70);
+        cross.setBounds(80,50,70,70);
+        percent.setBounds(150,50,70,70);
+        divide.setBounds(220,50,70,70);
+        seven.setBounds(10,120,70,70);
+        eight.setBounds(80,120,70,70);
+        nine.setBounds(150,120,70,70);
+        mult.setBounds(220,120,70,70);
+        four.setBounds(10,190,70,70);
+        five.setBounds(80,190,70,70);
+        six.setBounds(150,190,70,70);
+        minus.setBounds(220,190,70,70);
+        one.setBounds(10,260,70,70);
+        two.setBounds(80,260,70,70);
+        three.setBounds(150,260,70,70);
+        plus.setBounds(220,260,70,70);
+        no.setBounds(10,330,70,70);
+        zero.setBounds(80,330,70,70);
+        decimal.setBounds(150,330,70,70);
+         result.setBounds(220,330,70,70);
+        f.add(tf);
+        f.add(clear);
+        f.add(cross);
+        f.add(percent);f.add(divide);f.add(seven);f.add(eight);f.add(nine);f.add(mult);f.add(four);
+        f.add(five);f.add(six);f.add(minus);f.add(one);f.add(two);f.add(three);f.add(plus);
+        f.add(no);f.add(zero);f.add(decimal);f.add(result);
+
+     f.setSize(300,450);
+        f.setLayout(null);
+        f.setVisible(true);
+
+
+    }
 }
